479d4546e142b06710415b39315301ffc558e443
YoshiDegent <yoshi_degent@hotmail.com>
2014-05-29 16:41:40 +0200

    First commit oef1 done!

---
+++ Oef1/Controllers/SignUpController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oef1.Controllers
+{
+    public class SignUpController : Controller
+    {
+        // GET: SignUp
+        public ActionResult Index(string email, string pw)
+        {
+            if(!string.IsNullOrWhiteSpace(email) && !string.IsNullOrWhiteSpace(pw))
+            {
+                ViewBag.Bericht = string.Format("Sign up successful for {0}!", email);
+            }
+            else
+            {
+                ViewBag.Bericht = "";
+            }
+            return View();
+        }
+
+        public ActionResult SignUp(string email, string pw)
+        {
+            if (!string.IsNullOrWhiteSpace(email) && !string.IsNullOrWhiteSpace(pw))
+            {
+                ViewBag.Bericht = string.Format("Sign up successful for {0}!", email);
+            }
+            else
+            {
+                ViewBag.Bericht = "";
+            }
+            return View();
+        }
+    }
+}
---
+++ Oef1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Oef1.MvcApplication" Language="C#" %>
